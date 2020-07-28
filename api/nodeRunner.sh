9c66822f1a9b861d45a50b96e6bb1ceb13a44cfd
alexnathanson <alex@alexnathanson.com>
2020-07-28 09:04:09 -0400

    nodeRunner spelling

--- api/nodeRunner.sh
+++ api/nodeRunner.sh
@@ -2,6 +2,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/load-balancer-with-API-RaspberryPi/api
+cd home/pi/load-balancer-with-API-for-RaspberryPi/api
 nodejs index.js
 cd /
